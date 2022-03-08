b911e9e10d4a61b46e7377f57714f771d24fa41c
Melih Cengelli <melihcengelli.tr@gmail.com>
2022-03-08 22:10:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ function TabPanel(props) {
 }
 
 
+
 function Auth({setIsLogged}) {
   const [registerFormData, setRegisterFormData] = useState({})
   const [loginFormData, setLoginFormData] = useState({})
@@ -77,6 +78,7 @@ function Auth({setIsLogged}) {
     .then((response) => {
       document.cookie = `token=${response.data.token}`;
       setIsLogged(true)
+      
 
     })
   }
@@ -153,12 +155,22 @@ function getCookie(name) {
 
 
 function Content() {
+  const url = "http://localhost:80"
+  const ntoken = (document.cookie).split("").splice(6);
+  let nst="";
+  ntoken.map(
+    (response) => nst=nst+response
+  )
+  const config = {
+    headers: { Authorization: `Bearer ${nst}` },
+  };
+
   const [value, setValue] = React.useState(0);
   const handleChange2 = (event, newValue) => {
     setValue(newValue);
   }
 
- 
+  
   
 
   const [todo, setTodo] = useState([
@@ -177,11 +189,27 @@ function Content() {
     const newstatue = document.getElementById("statueselect").innerText
     if (todo.length>0) {
       setTodo([...todo,{name:newtodo,category:newcategory, statue:newstatue,id:countId}])
-      setCountId(countId+1)
+      const tododata = {
+        "title":newtodo,
+        "categoryId":countId,
+        "statusId":countId,
+      }
+      axios
+      .post(url+"/todo",tododata,config)
+      .then(refreshtodo,setCountId(countId+1)
+      )
     }
     else {
       setTodo([{name:newtodo,category:newcategory, statue:newstatue,id:countId}])
-      setCountId(countId+1)
+      const tododata = {
+        "title":newtodo,
+        "categoryId":newcategory,
+        "statusId":newstatue,
+      }
+      axios
+      .post(url+"/todo",tododata,config)
+      .then(refreshtodo,setCountId(countId+1))
+
     }
     
   }
@@ -192,32 +220,89 @@ function Content() {
   
   }
 
-  const handleCategoryAdd = () => {
-    const getcategorytest = axios.get('http://localhost:80/category')
-    console.log(getcategorytest)
-    const newcategory = document.getElementById("addcategory").value
-    const postcategory = 
-    {
-      title:newcategory
-    }
-
-    axios
-    .post('http://localhost:80/category', postcategory)
-    
-    setCategory([...category,{category:newcategory,id:categoryCountId}])
-    setCategoryCountId(categoryCountId+1)
-    console.log(category)
-    categoryControl();
-
-  }
 
   
 
+  
+  
+  const handleCategoryAdd = () => {
+  
+    const newcategory = document.getElementById("addcategory").value
+    console.log("newcategory="+newcategory)
+    console.log(config)
+    axios
+    .post(url+"/category", {"title":`${newcategory}`}, {
+      headers: {
+        Authorization: `Bearer ${nst}`,
+
+      },
+    })
+    .then(refreshcate)
+    
+    
+    
+
+  }
+  useEffect(() => {
+    axios
+    .get(url+"/category", {headers: {Authorization: `Bearer ${nst}`,},})
+    .then((response) => (setCategory((response.data))))
+  },[]);
+
+  useEffect(() => {
+    axios
+    .get(url+"/todo", {headers: {Authorization: `Bearer ${nst}`,},})
+    .then((response) => (setTodo((response.data))))
+  },[]);
+  useEffect(() => {
+    category.map((response1)=> (
+      axios
+      .get(url+"/status?categoryId="+response1.id, {headers: {Authorization: `Bearer ${nst}`,},})
+      .then((response) => (setStatue(([...statue,response.data]))))
+      ))
+  },[]);
+
+
+
+
+  function refreshcate () {
+    axios
+    .get(url+"/category", {headers: {Authorization: `Bearer ${nst}`,},})
+    .then((response) => (setCategory((response.data))))
+  }
+  function refreshtodo () {
+    axios
+    .get(url+"/todo", {headers: {Authorization: `Bearer ${nst}`,},})
+    .then((response) => (setTodo((response.data))))
+  }
+  function refreshstatus () {
+    axios
+    .get(url+"/status?", {headers: {Authorization: `Bearer ${nst}`,},})
+    .then((response) => (setStatue((response.data))))
+  }
+
+
+
   const handleStatueAdd = () => {
     const newstatue = document.getElementById("addstatue").value
-    setStatue([...statue,{statue:newstatue,id:statueCountId}])
-    setStatueCountId(statueCountId+1)
     console.log(statue)
+
+    axios
+    .post(url+"/status", {"title":`${newstatue}`,"categoryId":`${2}`,"color":`${"white"}`}, {
+      headers: {
+        Authorization: `Bearer ${nst}`,
+
+      },
+    })
+    .then(
+      (response) => {console.log(response)},
+      axios
+      .get(url+"/status", {headers: {Authorization: `Bearer ${nst}`,},})
+      .then((response) => setStatue((response.data)),
+      )
+    
+      )
+
   }
 
   const handleTodoRemove = (event) => {
@@ -225,11 +310,8 @@ function Content() {
     for (let i=0; i<todo.length; i++) {
       if (todo[i].id==event.target.name) {
         indekstarget = i;
-        console.log("içeri girildi")
-        console.log("içeri girildiğinde todo"+todo)
       }
     }
-    console.log("silinen indeks"+indekstarget)
     const indekstodo = event.target.name
     todo.splice(indekstarget,1)
     setTodo([...todo])
@@ -241,12 +323,15 @@ function Content() {
     let categoryindekstarget;
     for (let i=0; i<category.length; i++) {
       if (category[i].id == event.target.name) {
-        categoryindekstarget= i;
+        categoryindekstarget= category[i].id;
       }
     }
-    category.splice(categoryindekstarget,1)
-    setCategory([...category])
+    axios
+    .delete(url+"/category/"+categoryindekstarget , {headers: {Authorization: `Bearer ${nst}`,},})
+    .then(refreshcate)
+    
   }
+
   const handleStatueRemove = (event) => {
     let statueindekstarget;
     for (let i=0; i<statue.length; i++) {
@@ -269,8 +354,25 @@ function Content() {
   const handleStatueChange = (event) => {
     setStatueSection(event.target.value);
   };
-  
 
+  const addstatuenew = (event) => {
+    const addtext = document.getElementById("addnewstatue"+event.currentTarget.id).value
+    setStatue([...statue,{
+      "title":addtext,
+      "categoryId":event.currentTarget.id,
+      "color":"purple"
+    }])
+    const statusdata= {
+      "title":addtext,
+      "categoryId":event.currentTarget.id,
+      "color":"purple"
+    };
+    axios
+    .post(url+"/status?categoryId="+event.currentTarget.id,statusdata,config)
+    .then(refreshstatus)
+  }
+  
+  const [age, setAge] = React.useState('');
  return (
   <div className="Content">
     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -302,7 +404,7 @@ function Content() {
               onChange={handleChange}
             > {category.map((category) => (
 
-                <MenuItem value={category.category}>{category.category}</MenuItem>
+                <MenuItem id={category.id} value={category.title}>{category.title}</MenuItem>
               
               ))}
 
@@ -320,7 +422,7 @@ function Content() {
               onChange={handleStatueChange}
             >
               {statue.map((statue) => (
-                <MenuItem value={statue.statue}>{statue.statue}</MenuItem>
+                <MenuItem value={statue.title}>{statue.title}</MenuItem>
               ))}
             </Select>
           </FormControl>
@@ -377,18 +479,28 @@ function Content() {
               <TableHead>
                 <TableRow>
                   <TableCell>Categories</TableCell>
+                  <TableCell align="right">Add Statue</TableCell>
                   <TableCell align="right">Categories Remove</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>                  
                     {category.map((category) => (
                       <TableRow
-                        key={category.category}
+                        key={category.title}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                       >
                         <TableCell component="th" scope="row">
-                          {category.category}
+                          {category.title}
                         </TableCell>
+
+
+                        <TableCell align="right" component="th" scope="row">
+                          <TextField  id={"addnewstatue"+category.id} label="Add Statue" variant="standard" />
+                          <Button  onClick={addstatuenew} variant="contained" id={category.id}>Add </Button>
+                        </TableCell>
+
+
+
                         <TableCell align="right"><Button onClick={handleCategoryRemove} variant="contained" name={category.id}>Category Remove</Button></TableCell>
                       </TableRow>
                     ))
@@ -402,8 +514,6 @@ function Content() {
       </TabPanel>
 
       <TabPanel value={value} index={2}>
-        <TextField name="newstatue" id="addstatue" label="Add Statue" variant="filled" type="text" />  
-        <Button onClick={handleStatueAdd} variant="contained">Ekle</Button>
 
         <TableContainer component={Paper}>
           <span>
@@ -421,7 +531,7 @@ function Content() {
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                       >
                         <TableCell component="th" scope="row">
-                          {statue.statue}
+                          {statue.title}
                         </TableCell>
                         <TableCell align="right"><Button onClick={handleStatueRemove} variant="contained"  name={statue.id}>Statue Remove</Button></TableCell>
                       </TableRow>
